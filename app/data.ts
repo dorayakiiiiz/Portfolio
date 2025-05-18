aad25352c7a06342724f91a6628f0a7a07151899
Dora <travansy2305@gmail.com>
2025-05-18 16:11:25 +0700

    Update data.ts

--- app/data.ts
+++ app/data.ts
@@ -32,11 +32,11 @@ export const EMAIL = ''
 export const WEB_TITLE = 'Name - Personal Website'
 export const WEB_DESCRIPTION =
   'A personal website showcasing my work and projects.'
-export const FOOTER_COPYRIGHT = '© 2025 Nguyen Van B.'
+export const FOOTER_COPYRIGHT = '© 2025 Tra Van Sy.'
 export const FOOTER_LINK = 'https://github.com/username/'
 
-export const NAME = 'Nguyen Van B'
-export const JOB_TITLE = 'NASA Frontend Intern'
+export const NAME = 'Tra Van Sy'
+export const JOB_TITLE = 'Student'
 export const DESCRIPTION =
   'I am a passionate designer and developer with a focus on creating beautiful and functional user interfaces. I have experience in design engineering, front-end development, and product design. I love to explore the intersection of design and technology, and I am always looking for new challenges to tackle.'
 
